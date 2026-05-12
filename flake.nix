050caefdd15b3815581123cb3bd437c9493b09b4
li <li@goldragon.criome.net>
2026-05-12 16:24:47 +0200

    Use shared stable Rust toolchain

--- flake.nix
+++ flake.nix
@@ -20,10 +20,13 @@
         system:
         let
           pkgs = import nixpkgs { inherit system; };
-          toolchain = fenix.packages.${system}.fromToolchainFile {
-            file = ./rust-toolchain.toml;
-            sha256 = "sha256-gh/xTkxKHL4eiRXzWv8KP7vfjSk61Iq48x47BEDFgfk=";
-          };
+          toolchain = fenix.packages.${system}.stable.withComponents [
+            "cargo"
+            "rustc"
+            "rustfmt"
+            "clippy"
+            "rust-src"
+          ];
           craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
           src = craneLib.cleanCargoSource ./.;
           cargoVendorDir = craneLib.vendorCargoDeps { inherit src; };
